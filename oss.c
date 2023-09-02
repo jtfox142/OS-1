bb41b3f65288cc8c1e7229c08eb83ad03c7ebc41
JT Fox <jtfox142@gmail.com>
2023-09-02 17:47:45 -0500

    Moved helper function into oss.c

--- oss.c
+++ oss.c
@@ -3,7 +3,12 @@
 #include<sys/wait.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include"oss.h"
+
+void help() {
+        printf("This program is designed to have a parent process call a child process.\n");
+        printf("The executable takes three flags: [-n proc], [-s simul], and [-t iter].\n");
+        printf("Add more here when you know the specifics of how your program will run.");
+}
 
 int main(int argc, char** argv) {
 	int option;
