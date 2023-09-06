ed835ce150a26c042333e6f16fb74a6dc345f4ec
JT Fox <jtfox142@gmail.com>
2023-09-05 21:14:15 -0500

    iter is working properly

--- oss.c
+++ oss.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
 	int option;
 	int proc;
 	int simul;
-	int iter;
+	char *iter;
 	while ((option = getopt(argc, argv, "hn:s:t:")) != -1) {
   		switch(option) {
    			case 'h':
@@ -27,22 +27,22 @@ int main(int argc, char** argv) {
 				simul = atoi(optarg);
 				break;
 			case 't':
-				iter = atoi(optarg);
+				iter = optarg;
 				break;
 		}
 	}
 	pid_t childPid = fork(); // This is where the child process splits from the parent
+	printf("proc: %d\n", proc);
+	printf("simul: %d\n", simul);
+	printf("iter: %s\n", iter);
+
 	if (childPid == 0) {
-		printf("I am a child but a copy of parent! My parent's PID is %d, and my PID is %d\n",
-    		getppid(), getpid());
-    		char* args[] = {"./worker", "Hello", "there", "exec", "is", "neat", NULL};
-    		//execvp(args[0], args);
-    		execlp(args[0],args[0],args[1],args[2],args[3],args[4],args[5],args[6],NULL);
+		printf("Child pid detected, sending to child\n");
+		execlp("./worker", iter, NULL);
     		fprintf(stderr,"Exec failed, terminating\n");
     		exit(1);
   	} else {
-    		printf("I'm a parent! My pid is %d, and my child's pid is %d \n",
-    		getpid(), childPid);
+    		printf("Back in parent\n");
     		//sleep(1);
     		wait(0);
   	}
--- worker.c
+++ worker.c
@@ -3,12 +3,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(int argc, char** argv) {
-  printf("Hello from Child.c, a new executable!\n");
-  printf("My process id is: %d\n",getpid());
-  printf(" I got %d arguments: \n", argc);
+  printf("Inside of child process\n");
+  int iterations;
+  iterations = atoi(argv[0]);
   int i;
-  for (i =0; i < argc; i++)
-    printf("|%s| ", argv[i]);
+  for (i =0; i < iterations; i++)
+    printf("|%d| ", i);
   printf("\nChild is now ending.\n");
   sleep(3);
   return EXIT_SUCCESS;
